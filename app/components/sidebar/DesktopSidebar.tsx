4783cf403a84bf8899c047ed3e67a8d4306cfd49
nikhil <nikhilmahato3337@gmail.com>
2024-05-09 16:29:03 +0530

    avatar button

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -4,6 +4,7 @@ import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
+import Avatar from "../Avatar";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -72,7 +73,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
        transition
        "
         >
-          <Avatar />
+          <Avatar user={currentUser} />
         </div>
       </nav>
     </div>
